cb50745659664a94d41f3134fc87f773a7da736f
mee <106035102+JennaTolls78@users.noreply.github.com>
2023-04-03 13:41:39 -0400

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,25 @@
+import requests as req, base64
+
+
+r = req.session()
+login_url = "https://public-ubiservices.ubi.com/v3/profiles/sessions"
+
+email = "gmail.com"
+password = ""
+
+account_login = f'{email}:{password}'
+login_encoded = ((account_login).encode('ascii'))
+auth = (base64.b64encode(login_encoded)).decode('ascii')
+auth_headers = {
+    'Authorization': 'Basic ' + auth,
+    'Ubi-AppId': '2c2d31af-4ee4-4049-85dc-00dc74aef88f',
+    'Ubi-RequestedPlatformType': 'uplay',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+    'Referer': 'https://connect.ubisoft.com/'
+    }
+auth_payload = {'rememberMe': 'false'}
+auth_request = r.post(login_url, headers=auth_headers, json=auth_payload)
+auth_response = auth_request.json()
+auth_token = auth_response['ticket']
+auth_sessionid = auth_response['sessionId']
+print (f'Auth token generated')
