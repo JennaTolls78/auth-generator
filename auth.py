0e7ce1b5e371c5b5d25a7c7048cde134dc6461bb
mee <106035102+rarehp@users.noreply.github.com>
2023-04-24 14:00:40 -0400

    Update auth.py

--- auth.py
+++ auth.py
@@ -4,12 +4,15 @@ import requests as req, base64
 r = req.session()
 login_url = "https://public-ubiservices.ubi.com/v3/profiles/sessions"
 
+# Ubi account needed to generate token
 email = "gmail.com"
 password = ""
 
 account_login = f'{email}:{password}'
 login_encoded = ((account_login).encode('ascii'))
 auth = (base64.b64encode(login_encoded)).decode('ascii')
+
+# Headers and payload for sending request
 auth_headers = {
     'Authorization': 'Basic ' + auth,
     'Ubi-AppId': '2c2d31af-4ee4-4049-85dc-00dc74aef88f',
@@ -18,8 +21,11 @@ auth_headers = {
     'Referer': 'https://connect.ubisoft.com/'
     }
 auth_payload = {'rememberMe': 'false'}
+# Send request
 auth_request = r.post(login_url, headers=auth_headers, json=auth_payload)
+
+# Get token and sessionid from the response
 auth_response = auth_request.json()
 auth_token = auth_response['ticket']
 auth_sessionid = auth_response['sessionId']
-print (f'Auth token generated')
+print (f'{auth_token}')
